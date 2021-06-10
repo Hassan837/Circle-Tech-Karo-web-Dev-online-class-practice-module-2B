8ffdb0cc32a88de151462ddd0b5e8b9ef40f9efb
Hassan <hassanirfan429@gmail.com>
2021-06-11 01:28:52 +0500

    dropdown value

--- Class practice/app.js
+++ Class practice/app.js
@@ -1,31 +1,16 @@
-// convert Celsius in to Fahrenheit
-var t = document.querySelector('#inp1').value;
 
+var temp = document.querySelector('#inp1').value;
+var Cdeg = ( temp - 32) / 1.8;
+var Fdeg = (1.8 * temp) + 32;
+var C = document.querySelector('#cel').value == 'C';
 
-if(document.getElementById("temp").value == C) {
-    let f = (1.8 * t) + 32;
-    let answer = document.querySelector('#res').value = f;
+if (document.querySelector('#temp').value == C){
 
-    console.log(answer);
+    console.log(Celsius);
+
 }
 
-
-// function convert(){
-//     var F = document.querySelector('#inp1').value;
-//          F = (1.8 * C) + 32;
-
-// document.querySelector('#inp1').innerHTML = 'The Value of Celsius is 60 degree after converting into fahrenheit is =  ' + F+'C';
-// }
+ 
 
 
 
-// // convert Fahrenheit into celsius
-// function convert2(){
-//     var C ; 
-    
-//     var F = 98;
-    
-//     C = (F - 32) / 1.8;
-    
-//     document.querySelector('#inp2').innerHTML = 'The Value of Fahrenheit is 98 degree after converting into Celsius is =  ' + C+'F';
-//     }
