0d012f2e17a0f5e5a673e1aa524355b348228190
Hassan <hassanirfan429@gmail.com>
2021-06-11 04:22:50 +0500

    taking input

--- Class practice/app.js
+++ Class practice/app.js
@@ -1,13 +1,18 @@
 function convert() {
-    var temp = document.getElementById('temp').value;
+    var temp = document.getElementById('temp1').value;
     var cdeg = (temp * 9 / 5) + 32;
     var fdeg = (temp - 32) * 5 / 9;
 
-    if (document.getElementById('formula').value == 'C') {
+    if (document.getElementById('formula').value === 'C') {
         console.log(cdeg);
-    } else if (document.getElementById('formula').value == 'F') {
+        parseInt(document.querySelector('#res').value = cdeg + " " + "F");
+     } 
+    else if (document.getElementById('formula').value === 'F') {
         console.log(fdeg);
-    } else {
+        parseInt(document.querySelector('#res').value = fdeg + " " + "C");
+        
+    } 
+    else {
         alert("Select Unit First")
     }
 }
